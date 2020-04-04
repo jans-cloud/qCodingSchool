17719c0e4ceb5f656247c91f6eddee95a094d54d
Jan K <>
2020-04-04 03:33:53 +0200

    change frontend layout

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -1,17 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition, state } from '@angular/animations';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [
+    trigger('formInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2s', style({ opacity: 1 })),
+      ])
+    ]),
+    trigger('openClose', [
+      state('open', style({
+        opacity: 1,
+      })),
+      state('closed', style({
+        opacity: 0,
+      })),
+      transition('open => closed', [
+        animate('2s')
+      ]),
+      transition('closed => open', [
+        animate('1s')
+      ]),
+    ]),
+   ],
 })
 export class LandingComponent implements OnInit {
+  cardOne = true;
+  cardTwo = true;
+  cardThree = true;
+  cardFour = true;
+  state = "enter";
 
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  changeFormOne() {
+    this.cardOne = !this.cardOne;
+  }
+
+  changeFormTwo() {
+    this.cardTwo = !this.cardTwo;
+  }
+
+  changeFormThree() {
+    this.cardThree = !this.cardThree;
+  }
+
+  changeFormFour() {
+    this.cardFour = !this.cardFour;
   }
 
 }
