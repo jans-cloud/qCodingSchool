7be743d1622a66f4444bab8634585e46bcfd42c1
Jan K <>
2020-04-05 22:57:29 +0200

    change middleware order

--- backend/src/middleware/appMiddleware.ts
+++ backend/src/middleware/appMiddleware.ts
@@ -11,8 +11,8 @@ import logRequest from './requestLogger';
 import config from '../utils/config/appMiddleware';
 
 function setAppMiddleware(app: Application): void {
-  app.use(cors());
   app.use(helmet());
+  app.use(cors());
   app.use(bodyParser.urlencoded(config.bodyparserUrlConf));
   app.use(bodyParser.json());
   app.use(logRequest);
